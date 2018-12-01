a0396e1a672215f0cb3e26a4a64526172dbc96b1
vabook <vabook@163.com>
2018-12-01 23:52:30 +0800

    新增系统用户类

---
+++ ems/src/top/vabook/domain/User.java
@@ -0,0 +1,27 @@
+package top.vabook.domain;
+
+public class User {
+
+	private String userName;
+	private String password;
+	
+	public User(String username,String password) {
+		this.userName = username;
+		this.password = password;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
